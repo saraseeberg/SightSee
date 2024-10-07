83835e5a54a36dec9de9c0d5811e4bef28763c59
madsab <mads.barnes@hotmail.com>
2024-10-07 16:11:09 +0200

    add: layout for application

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { HashRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Error404 from "./pages/Error404"
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Error404 from './pages/Error404'
+import MainLayout from './layouts/MainLayout'
 
 // The app uses a hashrouter, therefore navigating to different routes use /#/Browse or /#/Search
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Error404 />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<Error404 />} />
+        </Route>
       </Routes>
     </HashRouter>
   )
