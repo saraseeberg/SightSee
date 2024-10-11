10ded316be83125cc0b7987419cf99f79383439c
madsab <mads.barnes@hotmail.com>
2024-10-11 14:28:45 +0200

    fix: margin for screens above 1536px

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <main className="bg-background min-h-screen text-content transition-all flex flex-col">
       <Navbar />
-      <div className=" flex-1">
+      <div className="flex-1 2xl:mx-[10%]">
         <Outlet />
       </div>
       <Footer />
