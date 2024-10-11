d3e84f0b11274b0601b1893b14714ef14a4b8fb1
madsab <mads.barnes@hotmail.com>
2024-10-11 16:08:31 +0200

    add: global Location type

---
+++ src/lib/types/Location.ts
@@ -0,0 +1,12 @@
+export interface Location {
+  id: number
+  title: string
+  titleQuestion?: string
+  description: string
+  categories: string[]
+  country: string
+  region?: string
+  image: string
+  alt: string
+  rating: number
+}
