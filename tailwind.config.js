d5194b5f37c791ed4ca482dcb32ea56b1f91b712
madsab <mads.barnes@hotmail.com>
2024-10-16 13:05:13 +0200

    add: squeeze and strech animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,12 +55,23 @@ export default {
       },
       animation: {
         float: 'float 3s infinite ease-in-out',
+        "squeeze-x": "squeeze-x 0.2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+        "stretch-x": "strecth-x 0.2s cubic-bezier(0.550, 0.085, 0.680, 0.530) both "
+
       },
       keyframes: {
         float: {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(-10%)' },
         },
+        "squeeze-x" : {
+          "0%": { transform: "scaleX(1)" },
+          "100%": { transform: "scaleX(0)" }
+        },
+        "strecth-x" : {
+          "0%": { transform: "scaleX(0)" },
+          "100%": { transform: "scaleX(1)" }
+        }
       },
     },
   },
