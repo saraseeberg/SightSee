c524cac814020c8e516ad0f38f425b434131cfd2
Aurora Nergaard <aurorane@stud.ntnu.no>
2024-10-25 11:18:37 +0200

    refactor: :rotating_light: fix lint

--- backend/src/models/destination.model.ts
+++ backend/src/models/destination.model.ts
@@ -1,43 +1,43 @@
 import { gql } from 'apollo-server-express'
 
 const Destination = gql`
-    type Destination {
-        id: Int!
-        title: String!
-        titleQuestion: String
-        description: String!
-        categories: [String]!
-        country: String!
-        region: String
-        image: String!
-        alt: String!
-        rating: Float!
-    }
+  type Destination {
+    id: Int!
+    title: String!
+    titleQuestion: String
+    description: String!
+    categories: [String]!
+    country: String!
+    region: String
+    image: String!
+    alt: String!
+    rating: Float!
+  }
 
-    input DestinationInput {
-        title: String!
-        titleQuestion: String
-        description: String!
-        categories: [String]!
-        country: String!
-        region: String
-        image: String!
-        alt: String!
-        rating: Float!
-    }
+  input DestinationInput {
+    title: String!
+    titleQuestion: String
+    description: String!
+    categories: [String]!
+    country: String!
+    region: String
+    image: String!
+    alt: String!
+    rating: Float!
+  }
 
-    type Query {
-        getDestination(id: Int!): Destination
-        getAllDestinations: [Destination]
-    }
+  type Query {
+    getDestination(id: Int!): Destination
+    getAllDestinations: [Destination]
+  }
 
-    type Mutation {
-        createDestination(destination: DestinationInput): Destination
+  type Mutation {
+    createDestination(destination: DestinationInput): Destination
 
-        createDestinations(destinations: [DestinationInput!]!): [Destination!]!
-        
-        deleteDestination(id: Int!): Destination
-    }
+    createDestinations(destinations: [DestinationInput!]!): [Destination!]!
+
+    deleteDestination(id: Int!): Destination
+  }
 `
 
 export default Destination
