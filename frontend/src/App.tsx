91c12b8c1ebefb7fe08dbad23b6a512a6e224116
madsab <mads.barnes@hotmail.com>
2024-10-29 15:54:59 +0100

    mov: molecule components into molecules folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import Error404 from './pages/Error404'
 import MainLayout from './layouts/MainLayout'
 import Browse from './pages/Browse'
+import Profile from './pages/Profile'
 
 // The app uses a hashrouter, therefore navigating to different routes use /#/Browse or /#/Search
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="/Browse" element={<Browse />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
         <Route path="*" element={<Error404 />} />
       </Routes>
