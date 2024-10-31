b0e6dca6da20a5023e8a6efe69f5a3c54a1fbbee
Sara Seeberg-Rommetveit <sarasee@stud.ntnu.no>
2024-10-31 13:59:57 +0100

    style: :art: Changed name of CurrentRating to StarRating

--- frontend/src/components/molecules/CardDetailsDialog.tsx
+++ frontend/src/components/molecules/CardDetailsDialog.tsx
@@ -1,7 +1,7 @@
 import { Dialog, DialogContent, DialogDescription, DialogTitle } from '@/components/ui/dialog'
 import React from 'react'
 import { Link } from 'react-router-dom'
-import StarRating from './CurrentRating'
+import StarRating from './StarRating'
 import { Button } from '@/components/ui/button'
 import { Destination } from '@types'
 
