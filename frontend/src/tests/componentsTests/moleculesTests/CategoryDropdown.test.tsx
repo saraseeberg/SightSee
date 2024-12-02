d1b886535e7b0f515ba547e77624109556621e67
Lotte Kvalheim <lottek@stud.ntnu.no>
2024-12-02 20:56:23 +0100

    fixed tests

--- frontend/src/tests/componentsTests/moleculesTests/CategoryDropdown.test.tsx
+++ frontend/src/tests/componentsTests/moleculesTests/CategoryDropdown.test.tsx
@@ -1,29 +1,17 @@
 import CategoryDropdown from '@/components/molecules/CategoryDropdown'
 import '@testing-library/jest-dom'
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { describe, expect, it, vi } from 'vitest'
 
 describe('CategoryDropdown', () => {
-  const mockOnSelectCategory = vi.fn()
+  const mockOnSelectCategories = vi.fn()
 
-  const renderComponent = () => render(<CategoryDropdown onSelectCategory={mockOnSelectCategory} />)
+  const renderComponent = () =>
+    render(<CategoryDropdown onSelectCategories={mockOnSelectCategories} selectedCategories={[]} />)
 
-  it('renders with the default selected category', () => {
+  it('renders with default label', () => {
     renderComponent()
 
-    const triggerButton = screen.getByRole('button', { name: /all/i })
-    expect(triggerButton).toBeInTheDocument()
-  })
-
-  it('does not call onSelectCategory when selecting the already selected category', () => {
-    renderComponent()
-
-    const triggerButton = screen.getByRole('button', { name: /all/i })
-    fireEvent.click(triggerButton)
-
-    const allItem = screen.getByText('All')
-    fireEvent.click(allItem)
-
-    expect(mockOnSelectCategory).toHaveBeenCalledTimes(0)
+    expect(screen.getByText('Select categories')).toBeInTheDocument()
   })
 })
