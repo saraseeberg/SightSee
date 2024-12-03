c67281e17bad4450ca2325a801dfc904a03b7e67
Aurora Nergaard <aurorane@stud.ntnu.no>
2024-12-03 16:17:57 +0100

    126 fix footer (#133)
    * refactor: Fix footer
    
    * style: :rotating_light: lint
    
    * Update package.json
    
    ---------
    
    Co-authored-by: Sara Seeberg-Rommetveit <sarasee@stud.ntnu.no>

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { Button } from './ui/button'
 import { Input } from './ui/input'
 
 const SoMeIcon = ({ icon, href }: { icon: string; href: string }) => (
-  <Link to={href} className="hover:scale-105  hover:text-secondary/80 ">
+  <Link to={href} className="hover:scale-105 hover:text-secondary/80 ">
     <Icon icon={icon} className="h-6 w-6" />
   </Link>
 )
@@ -19,7 +19,6 @@ const Footer = () => {
   return (
     <footer className="w-full bg-accent-1 flex max-md:flex-col px-16 py-12 gap-10 text-white mt-4">
       <section className="flex-1 space-y-5">
-        <p className="text-accent-2">IT'S FREE</p>
         <h1 className="text-2xl font-bold">Don't miss out on our newsletter</h1>
         <form onSubmit={handleSubscribe} className="flex max-md:flex-col items-center gap-2">
           <Input type="email" required placeholder="Your Email" />
@@ -27,6 +26,7 @@ const Footer = () => {
             Subscribe
           </Button>
         </form>
+        <p className="text-accent-2 mt-4">Developed by: Aurora Nergaard, Lotte Kvalhem, Mads Bårnes, Sara Seeberg</p>
       </section>
       <section className="flex-1 flex max-md:flex-col">
         <div className="flex-1 flex justify-around items-start">
@@ -34,17 +34,11 @@ const Footer = () => {
             <Link to="/browse">Browse</Link>
             <Link to="/reviews">Reviews</Link>
           </div>
-          <div className="text-accent-2 flex flex-col gap-6 group transition-all">
-            <p className="group-hover:-translate-x-48 group:pointer-events-none duration-150">Terms and conditions</p>
-            <p className={'group-hover:-translate-y-24 group:pointer-events-none duration-150'}>Privacy Policy</p>
-            <p className={'group-hover:translate-y-24 group:pointer-events-none duration-150'}>Cookie Policy</p>
-          </div>
         </div>
         <div className="flex md:flex-col justify-around my-3 gap-6">
           <SoMeIcon href="https://i.imgflip.com/70peq9.gif" icon="akar-icons:instagram-fill" />
           <SoMeIcon href="https://i.imgflip.com/70peq9.gif" icon="akar-icons:twitter-fill" />
           <SoMeIcon href="https://i.imgflip.com/70peq9.gif" icon="akar-icons:linkedin-fill" />
-          <SoMeIcon href="https://i.imgflip.com/70peq9.gif" icon="akar-icons:facebook-fill" />
         </div>
       </section>
     </footer>
