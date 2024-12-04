71d7726a56ba31d77834aa46a28ccd74d37168a3
Sara Seeberg-Rommetveit <sarasee@stud.ntnu.no>
2024-12-04 15:27:13 +0100

    feat: :sparkles: implement marquee with flights

---
+++ frontend/src/components/atoms/FlightMarquee.tsx
@@ -0,0 +1,18 @@
+import Marquee from '@/components/ui/marquee'
+import { Icon } from '@iconify/react'
+
+const flights = Array.from({ length: 8 }, (_, i) => ({ id: i + 1 }))
+
+export function FlightMarquee() {
+  return (
+    <div className="relative flex w-full items-center justify-center overflow-hidden">
+      <Marquee reverse pauseOnHover className="[--duration:15s]">
+        {flights.map((flight) => (
+          <Icon key={flight.id} icon="fa-solid:plane" className="text-background text-7xl mx-4" />
+        ))}
+      </Marquee>
+      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-accent-2"></div>
+      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-accent-2"></div>
+    </div>
+  )
+}
