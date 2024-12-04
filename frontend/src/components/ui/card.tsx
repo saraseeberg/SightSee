51cfafdd3edcea467083217f5c6f4065fc16a2ef
Aurora Nergaard <aurorane@stud.ntnu.no>
2024-12-04 10:41:23 +0100

    style: :recycle: Fix contrast

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -21,13 +21,13 @@ CardHeader.displayName = 'CardHeader'
 const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
   ({ className, ...props }, ref) => (
     <h3 ref={ref} className={cn('font-semibold leading-none tracking-tight', className)} {...props} />
-  ),
+  ), 
 )
 CardTitle.displayName = 'CardTitle'
 
 const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
   ({ className, ...props }, ref) => (
-    <p ref={ref} className={cn('text-sm text-muted-foreground', className)} {...props} />
+    <p ref={ref} className={cn('text-sm, text-content', className)} {...props} />
   ),
 )
 CardDescription.displayName = 'CardDescription'
